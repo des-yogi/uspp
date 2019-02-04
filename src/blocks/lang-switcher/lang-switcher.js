24ad209f39957ef697d4c2546b7dc91ac3bb3c0a
des-yogi <nuevo-design@mail.ru>
2019-02-04 19:37:01 +0200

    lang switcher in progress

--- src/blocks/lang-switcher/lang-switcher.js
+++ src/blocks/lang-switcher/lang-switcher.js
@@ -1,25 +1,77 @@
-/*(function () {
-  if (isMobile.any) {
+(function () {
+  var isMobile = window.isMobile.any;
+
+  if (isMobile) {
     console.log('This - mobile');
 
     var langSwitcher = document.querySelector('.lang-switcher');
     if (langSwitcher) {
-      var langsArr = langSwitcher.querySelectorAll('.lang-switcher__link');
+      var langsArr = langSwitcher.querySelectorAll('.lang-switcher__item');
+      var langLinks = langSwitcher.querySelectorAll('.lang-switcher__link');
       var currentLang = langSwitcher.querySelector('.active');
     }
 
+    var makeActive = function () {
+      langLinks.forEach(function (link) {
+        if (link.classList.contains('active')) {
+          link.removeAttribute('href');
+          link.parentElement.classList.add('active');
+        }
+      });
+    };
+
+    makeActive();
+
     var langSwitchOpener = function (e) {
-      e.preventDefault();
+      // e.preventDefault();
       for (var i = 0; i < langsArr.length; i++) {
-        //$(langsArr[i]).show(300);
-        langsArr[i].classList.toggle('active');
-        //langsArr[i].parentElement.style = 'order:-1';
+        langsArr[i].classList.toggle('open');
       }
-      console.log('Click');
+      // console.log('Click');
     };
 
     if (currentLang) {
-      currentLang.addEventListener('click', langSwitchOpener);
+      langSwitcher.addEventListener('click', langSwitchOpener);
     }
   }
-})();*/
+})();
+
+/*(function(){
+  var langWrapper = document.querySelector('.page-header__lang');
+  var langLinks = langWrapper.querySelectorAll('.lang-switcher__link');
+  var isMobile = window.isMobile.any;
+
+  var makeActive = function () {
+    langLinks.forEach(function (link) {
+        if (link.classList.contains('active')) {
+          link.removeAttribute('href');
+          link.parentElement.classList.add('active');
+        }
+    });
+  };
+
+  makeActive();
+
+  if(langWrapper && isMobile) {
+
+    langWrapper.addEventListener('click', function(event) {
+      // event.currentTarget.stopPropagation();
+      // event.stopPropagation();
+      // event.preventDefault();
+      langWrapper.classList.toggle('main-nav__lang-wrapper--open');
+    }, true)
+  } else if (langWrapper && !isMobile) {
+      langWrapper.addEventListener('mouseover', function() {
+        if (!langWrapper.classList.contains('main-nav__lang-wrapper--open')) {
+          langWrapper.classList.add('main-nav__lang-wrapper--open');
+        } else return;
+      })
+
+      langWrapper.addEventListener('mouseout', function() {
+        if (langWrapper.classList.contains('main-nav__lang-wrapper--open')) {
+          langWrapper.classList.remove('main-nav__lang-wrapper--open');
+        } else return;
+      })
+  }
+
+}());*/
--- src/js/global-script.js
+++ src/js/global-script.js
@@ -23,7 +23,69 @@
 
 
 (function(){
-  // code
+  const Page = document.querySelector('.page');
+  if (Page.classList.contains('no-js')) {
+    Page.classList.remove('no-js');
+    Page.classList.add('js');
+  }
+  return;
+}());
 
+(function(){
+  const searchForm = document.getElementById('search-form');
+  const searchSubmit = document.querySelector('.search__submit');
+  const searchField = document.querySelector('input[type=search]');
+  const searchHandler = function (e) {
+    e.preventDefault();
+    if (searchField.classList.contains('open') && searchField.value !== "") {
+      searchForm.submit();
+    } else if (!searchField.classList.contains('open')) {
+        searchField.classList.add('open');
+    } else {
+        searchField.classList.remove('open');
+    }
+  };
 
+  if (searchSubmit) {
+    searchSubmit.addEventListener('click', searchHandler);
+  }
+}());
+
+//iframe lazyLoad (b-lazy)
+(function(){
+  const bLazy = new Blazy({
+    container: '.media__video-wrapper'
+  });
+}());
+
+(function(){
+  const partners = new Swiper('.swiper-container', {
+    speed: 400,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    grabCursor: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpointsInverse: true,
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3
+    },
+    768: {
+      slidesPerView: 4
+    },
+    1024: {
+      slidesPerView: 5
+    }
+  });
 }());
