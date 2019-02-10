fa1ed75523ce6b364e225b59cd3931f6758c9e7f
des-yogi <nuevo-design@mail.ru>
2019-02-10 23:17:14 +0200

    Home menu experiments

--- src/blocks/main-nav/main-nav.js
+++ src/blocks/main-nav/main-nav.js
@@ -1,4 +1,4 @@
-(function($) {
+/*(function($) {
 
   $.fn.menumaker = function(options) {
 
@@ -9,20 +9,6 @@
       }, options);
 
       return this.each(function() {
-        //cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
-        /*$(this).find("#menu-button").on('click', function(){
-          $(this).toggleClass('menu-opened');
-          var mainmenu = $(this).next('ul');
-          if (mainmenu.hasClass('open')) {
-            mainmenu.hide().removeClass('open');
-          }
-          else {
-            mainmenu.show().addClass('open');
-            if (settings.format === "dropdown") {
-              mainmenu.find('ul').show();
-            }
-          }
-        });*/
 
         // Анимация открятия меню и субменю, скорость анимации задается кастомно
         $.fn.animateThisElem = function(durationValue) {
@@ -65,7 +51,7 @@
         cssmenu.find('li ul').parent().addClass('parent');
 
         var multiTg = function() {
-          cssmenu.find(".parent>a").after('<span class="submenu-button"></span>');
+          cssmenu.find(".parent>a").prepend('<span class="submenu-button"></span>');
           cssmenu.find('.parent>a').on('click', function() {
             event.preventDefault();
             $(this).siblings('span').toggleClass('submenu-opened');
@@ -129,19 +115,40 @@
 
     defaultPosition = linePosition = activeElement.position().left;
 
-    /*menuLine.css("width", lineWidth);
-    menuLine.css("left", linePosition);
 
-    $("#hmenu > ul > li").hover(function() {
-      activeElement = $(this);
-      lineWidth = activeElement.width();
-      linePosition = activeElement.position().left;
-      menuLine.css("width", lineWidth);
-      menuLine.css("left", linePosition);
-    },
-    function() {
-      menuLine.css("left", defaultPosition);
-      menuLine.css("width", defaultWidth);
-    });*/
   });
 })(jQuery);
+*/
+
+
+
+(function () {
+  var myOffside = offside( '#my-menu', {
+
+      // Global offside options: affect all offside instances
+      slidingElementsSelector: '#content-container', // String: Sliding elements selectors ('#foo, #bar')
+      disableCss3dTransforms: false,                    // Disable CSS 3d Transforms support (for testing purposes)
+      debug: true,                                      // Boolean: If true, print errors in console
+
+      // Offside instance options: affect only this offside instance
+      buttonsSelector: '#my-button, .menu-toggle',   // String: Offside toggle buttons selectors ('#foo, #bar')
+      slidingSide: 'left',                             // String: Offside element pushed on left or right
+      init: function(){},                               // Function: After init callback
+      beforeOpen: function(){},                         // Function: Before open callback
+      afterOpen: function(){},                          // Function: After open callback
+      beforeClose: function(){},                        // Function: Before close callback
+      afterClose: function(){
+        $('.offside-sliding-element').css({'transform':'translate3d(0, 0, 0)', 'transition':'none'});
+      },                         // Function: After close callback
+
+  });
+
+  var overlay = document.querySelector('.site-overlay');
+  overlay.addEventListener( 'click', window.offside.factory.closeOpenOffside );
+})();
+
+$(function () {
+
+    $('#mobile-nav').metisMenu();
+
+});
